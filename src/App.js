c1795f393bca039a4783c2f2bfe29a5de253cacd
alejandra salazar <alejandra.salazar@cardumen.cl>
2020-02-18 12:55:29 -0300

    User can now play the game

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ function App() {
 
   const words = ['casa','gato','mesa']
 
+  // Saves the random word chosen.
   const [randomWord, setRandomWord] = useState()
 
+  // Draws the word in the DOM, shows spaces for each letter.
   const [chosenWord, setChoose] = useState()
 
-  const [valueOfInput, setValueOfInput] = useState()
+  // Saves the value of the input when onChange is triggered.
+  const [valueOfInput, setValueOfInput] = useState('')
 
+  // Sets the "try word" button on disable, and updates to able when a letter is entered into the input above.
   const [buttonAvailability, setButtonAvailability] = useState(true)
 
+  // Saves the number of clicks of the "try word" button
   const [clicks, setClicks] = useState(0)
 
+  // Updates with messages for the user as they interact with the app.
   const [response, setResponse] = useState()
 
-  
+  // Keeps count of the guessed letters
+  const [guessed, setGuessed] = useState(1)
+
+  // Throws a random word and creates the spaces for each letter in the DOM.
   let sort = () => {
     let random = words[Math.floor(Math.random() * words.length)];
     setRandomWord(random)
@@ -29,12 +38,14 @@ function App() {
     setChoose(aux)
   }
 
-  let wroteInsideInput = () => {
+  // Gets the value of the input when onChange is triggered.
+  let wroteInsideInput = (event) => {
     setButtonAvailability(false)
-    // como valueOfInput esta undefined, decirle de alguna forma al value que se actualice cuando onchange se actualiza, y asignarle el string ingresado.
-    // setValueOfInput()
+    let tryLetter = event.target.value
+    setValueOfInput(tryLetter)
   }
 
+  // Main interactions
   let countClicks = () => {
     if(chosenWord === undefined){
       let haveToClickSortButtonFirst = 'Debes clickear el botón "Sortear palabra" antes de hacer click aquí...'
@@ -46,14 +57,20 @@ function App() {
           let clicksLeft = 'Te quedan ' + (5 - i) + ' oportunidades para adivinar!' 
           setResponse(clicksLeft)
           let checkLetter = () => {
-            console.log('entré a checkLetter')
             console.log('randomWord --> ', randomWord)
             console.log('valueOfInput: ', valueOfInput)
             for(let j = 0 ; j < randomWord.length ; j++){
-              console.log(randomWord[j])
               if(valueOfInput === randomWord[j]){
                 console.log('le achunté')
                 document.getElementById(j).style.color = '#ffffff'
+                let guessedCounter = guessed + 1
+                setGuessed(guessedCounter)
+                console.log('guessed: ', guessed)
+                if(guessed === randomWord.length){
+                  let youWon = 'Ganaste!'
+                  setResponse(youWon)
+                  setButtonAvailability(true)
+                }
               }
             }
           }
@@ -65,7 +82,16 @@ function App() {
       }
     }
   }
-  
+
+  // Restart the game
+  let playAgain = () => {
+    setRandomWord(null)
+    setChoose(null)
+    setValueOfInput('')
+    setButtonAvailability(true)
+    setClicks(0)
+    setResponse('')
+  }
 
   return (
     <div className="App">
@@ -76,17 +102,18 @@ function App() {
           {chosenWord}
         </ul>
         <p>Ingresa una letra para chequear que exista en la palabra:</p>
-        <input type="text" id="letter" value={valueOfInput} onChange={() => {wroteInsideInput()}}/>
+        <input type="text" id="letter" value={valueOfInput} onChange={(event) => {wroteInsideInput(event)}}/>
         <button 
           disabled={buttonAvailability}
           onClick={ () => {
-            // checkLetter()
             let clicked = clicks + 1
             setClicks(clicked)
             countClicks()
           }}
         >Chequea la letra</button>
         <p>{response}</p>
+        <button onClick={() => {playAgain()}}>Jugar de nuevo</button>
+
       </header>
     </div>
   );
